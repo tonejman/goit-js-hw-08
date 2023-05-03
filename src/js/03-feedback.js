1ef8a45cdf9262653a0c75c765ab4e61e6f81580
tonejman <nej.man@interia.pl>
2023-05-04 00:06:03 +0200

    task completed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('[name="email"]');
+const messageTextarea = document.querySelector('[name="message"]');
+
+const saveCurrentData = () => {
+  const currentData = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(currentData));
+};
+emailInput.addEventListener('input', throttle(saveCurrentData, 500));
+messageTextarea.addEventListener('input', throttle(saveCurrentData, 500));
+
+const savedCurrentData = JSON.parse(
+  localStorage.getItem('feedback-form-state')
+);
+
+if (savedCurrentData) {
+  emailInput.value = savedCurrentData.email;
+  messageTextarea.value = savedCurrentData.message;
+}
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const currentData = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+
+  emailInput.value = '';
+  messageTextarea.value = '';
+
+  localStorage.removeItem('feedback-form-state');
+
+  console.log(`
+  email: ${currentData.email}  
+  message: ${currentData.message}`);
+});
