22914737c57813276949e1eb8ca8fa4bb14b92ad
tonejman <nej.man@interia.pl>
2023-05-03 14:21:35 +0200

    task completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+function saveCurrentPlayTime(time) {
+  const { seconds } = time;
+  console.log(`${seconds}s`);
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+const throttleUpdateTime = throttle(saveCurrentPlayTime, 1000);
+player.on('timeupdate', throttleUpdateTime);
+const currentTime = localStorage.getItem('videoplayer-current-time');
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
