654d8d8c58c9dcb0d6ebce10fb0d79f557c1645a
tonejman <nej.man@interia.pl>
2023-05-03 14:50:28 +0200

    task completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,45 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+console.log(SimpleLightbox);
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const galleryEl = createGalleryEl(galleryItems);
+gallery.innerHTML = galleryEl;
+
+function createGalleryEl() {
+  return (
+    galleryItems
+
+      // eslint-disable-next-line arrow-body-style
+      .map(({ preview, original, description }) => {
+        return `<div 
+      class="gallery__item">
+      <a class="gallery__link"
+      href="${original}">
+      <img class="gallery__image"
+      src=" ${preview}" 
+      data-source="${original}" alt=" 
+      ${description}"/>
+      </a>
+      </div>`;
+      })
+      .join('')
+  );
+}
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+gallery.addEventListener('click', event => {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+});
